218a07aad3acf4e6ae9ec4833326e748dee40863
Christian Puhrsch <cpuhrsch@fb.com>
2021-12-01 20:33:25 -0500

    Rebuild MHA on top of recent changes

--- test/test_basics.py
+++ test/test_basics.py
@@ -107,6 +107,69 @@ class TestMaskedTensor(TestCase):
             x, ~(key_padding_mask.transpose(0, 1).unsqueeze(-1).expand_as(x))
         )
 
+    def test_mha_issue_41508(self):
+        # https://github.com/pytorch/pytorch/issues/41508
+        # TODO:
+        # 1. Restore matmul mask assert
+        # 2. masked_bmm + grad
+        # 3. Rename to masked_bmm
+        # 3. ...
+        import torch
+
+        torch.manual_seed(0)
+        attn_nn = torch.nn.MultiheadAttention(1, 1, bias=False)
+        attn_mt = torch.nn.MultiheadAttention(1, 1, bias=False)
+        for (na, a), (nb, b) in zip(
+            attn_nn.named_parameters(), attn_mt.named_parameters()
+        ):
+            a.data.copy_(b.data)
+
+        x = torch.rand(3, 2, 1)
+        key_padding_mask = torch.as_tensor(
+            [
+                [False, False, False],
+                [False, True, True],
+            ]
+        )
+        attn_mask = torch.as_tensor(
+            [
+                [False, True, True],
+                [False, False, True],
+                [True, False, False],
+            ]
+        )
+        output, scores = attn_nn(
+            x, x, x, key_padding_mask=key_padding_mask, attn_mask=attn_mask
+        )
+        print("")
+        print("0 scores")
+        print(scores)
+        loss0 = output[0, :].sum()
+        print("0 loss")
+        print(loss0)
+        loss0.backward()
+        print("0 grads")
+        for n, p in attn_nn.named_parameters():
+            print(0, n, p.grad)
+
+        print("")
+        x_mt = maskedtensor.masked_tensor(
+            x, ~(key_padding_mask.transpose(0, 1).unsqueeze(-1).expand_as(x))
+        )
+
+        output, scores = attn_mt(x, x_mt, x, attn_mask=attn_mask)
+        print("1 scores")
+        print(scores)
+        loss1 = output[0, :].sum()
+        print("1 loss")
+        print(loss1)
+        loss1.backward()
+        print("1 grads")
+        for n, p in attn_nn.named_parameters():
+            print(1, n, p.grad)
+
+        self.assertEqual(loss0, loss1.masked_data)
+
 
 if __name__ == "__main__":
     unittest.main()
